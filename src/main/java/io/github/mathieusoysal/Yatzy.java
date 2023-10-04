5e52596b9a8ae68f61c7202dbb43af6da8860f76
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-04 21:41:10 +0000

    refactor ThreeOfKind

--- src/main/java/io/github/mathieusoysal/Yatzy.java
+++ src/main/java/io/github/mathieusoysal/Yatzy.java
@@ -80,17 +80,8 @@ public class Yatzy {
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
-        int[] t;
-        t = new int[6];
-        t[d1 - 1]++;
-        t[d2 - 1]++;
-        t[d3 - 1]++;
-        t[d4 - 1]++;
-        t[d5 - 1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i + 1) * 3;
-        return 0;
+        DiceResults diceResults = new DiceResults(d1, d2, d3, d4, d5);
+        return YatzyMod.THREE_OF_A_KIND.calculate(diceResults);
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
--- src/main/java/io/github/mathieusoysal/yatzy_mod/YMThreeOfAKind.java
+++ src/main/java/io/github/mathieusoysal/yatzy_mod/YMThreeOfAKind.java
@@ -1,13 +1,19 @@
 package io.github.mathieusoysal.yatzy_mod;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import io.github.mathieusoysal.DiceResults;
 
 class YMThreeOfAKind implements YatzyModInterface {
 
     @Override
     public int calculate(DiceResults diceResults) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calculate'");
+        Collection<Integer> diceCollection = diceResults.getDicesIntStream()
+                .boxed().toList();
+        return diceResults.getDicesIntStream()
+                .filter(v -> Collections.frequency(diceCollection, v) >= 3)
+                .findAny().orElse(0) * 3;
     }
 
 }
