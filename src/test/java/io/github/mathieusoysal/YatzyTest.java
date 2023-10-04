065a088e2d9d0fc6b2b12fcaec87645729e6435c
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-04 18:14:07 +0000

    Refactor package of project

--- src/test/java/io/github/mathieusoysal/YatzyTest.java
+++ src/test/java/io/github/mathieusoysal/YatzyTest.java
@@ -1,7 +1,11 @@
+package io.github.mathieusoysal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import io.github.mathieusoysal.Yatzy;
 
 class YatzyTest {
 
