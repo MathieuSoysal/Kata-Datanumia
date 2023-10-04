af79fb924758ec150f3dc35ac6cbb7925bae23c6
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-04 15:45:38 +0000

    Add test for ones

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -32,6 +32,7 @@ class YatzyTest {
         assertEquals(2, Yatzy.ones(1, 2, 1, 4, 5));
         assertEquals(0, Yatzy.ones(6, 2, 2, 4, 5));
         assertEquals(4, Yatzy.ones(1, 2, 1, 1, 1));
+        assertEquals(0, Yatzy.ones(3, 3, 3, 4, 5));
     }
 
     @Test
