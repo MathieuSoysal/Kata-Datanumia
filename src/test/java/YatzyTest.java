d8827215f44bb2a3cb77e0ffaa2891fe004eb4a9
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-04 16:24:37 +0000

    Add test three_of_a_kind

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -97,6 +97,8 @@ class YatzyTest {
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5));
         assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5));
         assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5));
+        assertEquals(0, Yatzy.three_of_a_kind(3, 3, 4, 5, 6));
+        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 1));
     }
 
     @Test
