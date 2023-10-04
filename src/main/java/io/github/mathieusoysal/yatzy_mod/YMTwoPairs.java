e64b3585942ac588949b9f7690f81096b936274c
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-04 21:23:55 +0000

    Refactor twoPairs

--- src/main/java/io/github/mathieusoysal/yatzy_mod/YMTwoPairs.java
+++ src/main/java/io/github/mathieusoysal/yatzy_mod/YMTwoPairs.java
@@ -1,13 +1,21 @@
 package io.github.mathieusoysal.yatzy_mod;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import io.github.mathieusoysal.DiceResults;
 
 class YMTwoPairs implements YatzyModInterface {
 
     @Override
     public int calculate(DiceResults diceResults) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calculate'");
+        Collection<Integer> diceCollection = diceResults.getDicesIntStream()
+                .boxed().toList();
+        List<Integer> pairs = diceResults.getDicesIntStream()
+                .filter(v -> Collections.frequency(diceCollection, v) > 1)
+                .distinct().boxed().toList();
+        return pairs.size() == 2 ? (pairs.get(0) + pairs.get(1)) * 2 : 0;
     }
 
 }
