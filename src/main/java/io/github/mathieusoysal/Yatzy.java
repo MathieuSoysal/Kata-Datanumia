83c1da757e561209f3e47f2ecb12037f4946cc83
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-04 21:46:03 +0000

    refactor fourOfAKind

--- src/main/java/io/github/mathieusoysal/Yatzy.java
+++ src/main/java/io/github/mathieusoysal/Yatzy.java
@@ -65,18 +65,9 @@ public class Yatzy {
         return YatzyMod.TWO_PAIRS.calculate(diceResults);
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1 - 1]++;
-        tallies[_2 - 1]++;
-        tallies[d3 - 1]++;
-        tallies[d4 - 1]++;
-        tallies[d5 - 1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i + 1) * 4;
-        return 0;
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
+        DiceResults diceResults = new DiceResults(d1, d2, d3, d4, d5);
+        return YatzyMod.FOUR_OF_A_KIND.calculate(diceResults);
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
--- src/main/java/io/github/mathieusoysal/yatzy_mod/YMFourOfAKind.java
+++ src/main/java/io/github/mathieusoysal/yatzy_mod/YMFourOfAKind.java
@@ -1,13 +1,19 @@
 package io.github.mathieusoysal.yatzy_mod;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import io.github.mathieusoysal.DiceResults;
 
 class YMFourOfAKind implements YatzyModInterface {
 
     @Override
     public int calculate(DiceResults diceResults) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calculate'");
+        Collection<Integer> diceCollection = diceResults.getDicesIntStream()
+                .boxed().toList();
+        return diceResults.getDicesIntStream()
+                .filter(v -> Collections.frequency(diceCollection, v) >= 4)
+                .findAny().orElse(0) * 4;
     }
 
 }
