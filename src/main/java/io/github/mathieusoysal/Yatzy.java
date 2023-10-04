aef78ef87ed00ac7420df3310ebf690ffc5e16e6
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-04 19:38:30 +0000

    Refactor threes

--- src/main/java/io/github/mathieusoysal/Yatzy.java
+++ src/main/java/io/github/mathieusoysal/Yatzy.java
@@ -25,19 +25,8 @@ public class Yatzy {
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
-        int s;
-        s = 0;
-        if (d1 == 3)
-            s += 3;
-        if (d2 == 3)
-            s += 3;
-        if (d3 == 3)
-            s += 3;
-        if (d4 == 3)
-            s += 3;
-        if (d5 == 3)
-            s += 3;
-        return s;
+        DiceResults diceResults = new DiceResults(d1, d2, d3, d4, d5);
+        return YatzyMod.THREES.calculate(diceResults);
     }
 
     protected int[] dice;
