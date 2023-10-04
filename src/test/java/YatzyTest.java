81826718bc3036da4906fe090bfcf3e261a6f50b
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-04 17:13:28 +0000

    Organize import test

--- src/test/java/YatzyTest.java
+++ src/test/java/YatzyTest.java
@@ -1,8 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class YatzyTest {
 
