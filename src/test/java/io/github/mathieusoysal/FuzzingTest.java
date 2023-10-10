c272f68a105b2ef3669ed2c051b3b96b025a4a63
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-10-10 09:31:23 +0000

    Update CI for fuzzing test

--- src/test/java/io/github/mathieusoysal/FuzzingTest.java
+++ src/test/java/io/github/mathieusoysal/FuzzingTest.java
@@ -2,25 +2,12 @@ package io.github.mathieusoysal;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.code_intelligence.jazzer.junit.FuzzTest;
 
 import io.github.mathieusoysal.yatzy_mod.YatzyMod;
 
 class FuzzingTest {
-
-    @FuzzTest
-    void test_doesNotthrowsIncorrectDiceValue_withGoodValue(FuzzedDataProvider data) {
-        int d1 = data.consumeInt(1, 6);
-        int d2 = data.consumeInt(1, 6);
-        int d3 = data.consumeInt(1, 6);
-        int d4 = data.consumeInt(1, 6);
-        int d5 = data.consumeInt(1, 6);
-        assertDoesNotThrow(() -> new Dices(d1, d2, d3, d4, d5));
-    }
 
     @FuzzTest
     void test_calculateScore_withNotNullDices(FuzzedDataProvider data) {
